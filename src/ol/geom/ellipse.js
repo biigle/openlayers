fa651f10f649f3a3a00ba0b6550d5faa06d0e2dd
Martin Zurowietz <martin@zurowietz.de>
2018-09-11 13:22:06 +0200

    Fix ellipse area calculation
    References biigle/annotations#110

--- src/ol/geom/ellipse.js
+++ src/ol/geom/ellipse.js
@@ -51,3 +51,20 @@ ol.geom.Ellipse.prototype.closestPointXY = function(x, y, closestPoint, minSquar
 
   return distance;
 };
+
+/**
+ * Return the area of the ellipse.
+ * @return {number} Area
+ * @api
+ * @override
+ */
+ol.geom.Ellipse.prototype.getArea = function() {
+  var coords = this.flatCoordinates;
+  // Diameter along first principal axis.
+  var a = Math.sqrt(Math.pow(coords[0] - coords[4], 2) + Math.pow(coords[1] - coords[5], 2));
+  // Diameter along second principal axis.
+  var b = Math.sqrt(Math.pow(coords[2] - coords[6], 2) + Math.pow(coords[3] - coords[7], 2));
+
+  // Multiply by 0.25 because the area is calculated with the radius, not the diameter.
+  return Math.PI * a * b * 0.25;
+};
